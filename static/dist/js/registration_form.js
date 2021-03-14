6daa95a14a62db8ae941150fe1b852e0f68006f4
slow999 <slow999@gmail.com>
2021-03-14 00:08:32 -0600

    added step

--- static/dist/js/registration_form.js
+++ static/dist/js/registration_form.js
@@ -2,26 +2,36 @@
 
 const e = React.createElement;
 
+
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       firstName: '',
-      lastName: ''
+      lastName: '',
+      email: '',
+      current_step: 1,
+      total_steps: 2,
     };
     this.goToNextClick = this.goToNextClick.bind(this);
+    this.goToPreviousClick = this.goToPreviousClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    console.log(event.target);
-    console.log(event.target.value);
+    console.log(event.target.name);
     this.setState({[event.target.name]: event.target.value});
   }
 
   goToNextClick() {
+    event.preventDefault();
+    this.setState({current_step: this.state.current_step + 1});
+  }
 
+  goToPreviousClick() {
+    event.preventDefault();
+    this.setState({current_step: this.state.current_step - 1});
   }
 
   handleSubmit(event) {
@@ -30,9 +40,11 @@ class MyForm extends React.Component {
   }
 
   render() {
-    return (
-      <form className="form" method="post" onSubmit={this.handleSubmit}>
-        <CSRFToken />
+    const current_step = this.state.current_step;
+
+    let visiableFields;
+    if (current_step === 1) {
+      visiableFields =
         <div className="row">
           <div className="col-lg-3 col-md-3 first-name-wrapper">
             <div className="form-group">
@@ -45,12 +57,31 @@ class MyForm extends React.Component {
             <div className="form-group">
               <label htmlFor="id_last_name">Last Name</label>
               <input type="text" name="lastName" className="form-control" maxLength="32" required id="id_last_name"
-                     autoComplete="off" defaultValue={this.state.lastName}/>
+                     autoComplete="off" value={this.state.lastName} onChange={this.handleChange}/>
             </div>
           </div>
         </div>
-        <button className="btn btn-info" name="go-back" onClick={this.goToNextClick}>Go back</button>
-        <button className="btn btn-info" name="go-to-next">Next</button>
+    }
+    else if (current_step === 2){
+      visiableFields =
+        <div className="row">
+          <div className="col-lg-3 col-md-3 email-wrapper">
+            <div className="form-group">
+              <label htmlFor="id_email">Email</label>
+              <input type="text" name="email" className="form-control" maxLength="128" required id="id_email"
+                     autoComplete="off" value={this.state.email} onChange={this.handleChange}/>
+            </div>
+          </div>
+        </div>
+    }
+
+
+    return (
+      <form className="form" method="post" onSubmit={this.handleSubmit}>
+        <CSRFToken/>
+        {visiableFields}
+        <button className="btn btn-info" name="go-back" onClick={this.goToPreviousClick}>Go back</button>
+        <button className="btn btn-info" name="go-to-next" onClick={this.goToNextClick}>Next</button>
         <button type="submit" className="btn btn-primary" name="action" value="submit">Submit</button>
       </form>
     );
